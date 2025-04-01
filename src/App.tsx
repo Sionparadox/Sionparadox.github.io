cd0cd2e3f32e9a910e119fd3c67ab06a2084ae87
Sionparadox <sion9999@naver.com>
2025-04-01 17:56:48 +0900

    [design] 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ function App() {
     <div className='min-h-screen bg-gray-100 dark:bg-gray-900'>
       <header className='bg-white dark:bg-gray-800 shadow'>
         <div className='max-w-7xl mx-auto py-6 px-4'>
-          <h1 className='text-3xl font-bold text-gray-900 dark:text-white'>
+          <h1 className='text-3xl font-black text-gray-900 dark:text-white'>
             기술 블로그
           </h1>
         </div>
       </header>
       <main className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
         <div className='bg-white dark:bg-gray-800 shadow sm:rounded-lg p-6'>
-          <p className='text-gray-700 dark:text-gray-300'>초기화</p>
+          <p className='font-thin'>시온시온</p>
+          <p className='font-extralight'>시온시온</p>
+          <p className='font-light'>시온시온</p>
+          <p className='font-normal'>시온시온</p>
+          <p className='font-medium'>시온시온</p>
+          <p className='font-semibold'>시온시온</p>
+          <p className='font-bold'>시온시온</p>
+          <p className='font-extrabold'>시온시온</p>
+          <p className='font-black'>시온시온</p>
         </div>
       </main>
     </div>
