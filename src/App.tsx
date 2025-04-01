e82104c9176eb2fc8ef53742fc3201d98e59afc2
Sionparadox <sion9999@naver.com>
2025-04-01 18:20:26 +0900

    [design] 폰트 적용 수정 및 기본 색 명세 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './index.css';
 
 function App() {
   return (
-    <div className='min-h-screen bg-gray-100 dark:bg-gray-900'>
-      <header className='bg-white dark:bg-gray-800 shadow'>
+    <div className='min-h-screen '>
+      <header className=' shadow'>
         <div className='max-w-7xl mx-auto py-6 px-4'>
           <h1 className='text-3xl font-black text-gray-900 dark:text-white'>
             기술 블로그
@@ -12,15 +12,16 @@ function App() {
       </header>
       <main className='max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8'>
         <div className='bg-white dark:bg-gray-800 shadow sm:rounded-lg p-6'>
-          <p className='font-thin'>시온시온</p>
-          <p className='font-extralight'>시온시온</p>
-          <p className='font-light'>시온시온</p>
-          <p className='font-normal'>시온시온</p>
-          <p className='font-medium'>시온시온</p>
-          <p className='font-semibold'>시온시온</p>
-          <p className='font-bold'>시온시온</p>
-          <p className='font-extrabold'>시온시온</p>
-          <p className='font-black'>시온시온</p>
+          <p className='font-thin text-5xl'>시온시온</p>
+          <p className='font-extralight text-5xl'>시온시온</p>
+          <p className='font-light text-5xl'>시온시온</p>
+          <p className='font-normal text-5xl'>시온시온</p>
+          <p className='font-medium text-5xl'>시온시온</p>
+          <p className='font-semibold text-5xl'>시온시온</p>
+          <p className='font-bold text-5xl'>시온시온</p>
+          <p className='font-extrabold text-5xl'>시온시온</p>
+          <p className='font-black text-5xl'>시온시온</p>
+          <p>테스트트트</p>
         </div>
       </main>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,42 @@ export default {
       fontFamily: {
         sans: ['Pretendard', 'system-ui', '-apple-system', 'sans-serif'],
       },
+      colors: {
+        // Light mode colors
+        light: {
+          bg: {
+            main: '#FAFAFA',
+            card: '#E5E5E5',
+            sub: '#EDEDED',
+          },
+          text: {
+            main: '#404040',
+            title: '#171717',
+            gray: '#737373',
+            sub: '#525252',
+          },
+          stroke: {
+            main: '#D4D4D4',
+          },
+        },
+        // Dark mode colors
+        dark: {
+          bg: {
+            main: '#171717',
+            card: '#262626',
+            sub: '#363636',
+          },
+          text: {
+            main: '#b5b5b5',
+            title: '#e5e5e5',
+            gray: '#808080',
+            sub: '#a3a3a3',
+          },
+          stroke: {
+            main: '#404040',
+          },
+        },
+      },
     },
   },
   plugins: [],
