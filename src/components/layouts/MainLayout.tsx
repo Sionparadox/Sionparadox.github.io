7d39b481f765bb132f3b7bdb5c5a6b4a454d1b8d
Sionparadox <sion9999@naver.com>
2025-04-03 17:52:13 +0900

    [fix] 메인 레이아웃 깨짐 수정

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -12,23 +12,24 @@ export const MainLayout = () => {
   };
 
   return (
-    <div className="flex min-h-screen flex-col bg-white dark:bg-gray-900">
-      <div className="flex flex-1">
-        {/* 사이드바 */}
-        <div
-          className={`fixed inset-y-0 left-0 transform ${
-            isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
-          } z-30 transition-transform duration-300 ease-in-out`}
-        >
-          <Sidebar />
-        </div>
+    <div className="relative min-h-screen">
+      {/* 사이드바 */}
+      <div
+        className={`fixed left-0 top-0 h-full w-64 transform ${
+          isSidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        } z-30 transition-transform duration-300 ease-in-out`}
+      >
+        <Sidebar />
+      </div>
 
-        {/* 메인 콘텐츠 */}
-        <div
-          className={`flex min-h-screen flex-1 flex-col transition-all duration-300 ease-in-out ${
-            isSidebarOpen ? 'ml-64' : 'ml-0'
-          }`}
-        >
+      {/* 메인 콘텐츠 */}
+      <div
+        className={`min-h-screen transition-all duration-300 ease-in-out ${
+          isSidebarOpen ? 'pl-64' : 'pl-0'
+        }`}
+      >
+        {/* 실제 콘텐츠 */}
+        <div className="flex min-h-screen flex-col">
           <Header onToggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
           <main className="flex-1 p-6">
             <Outlet />
