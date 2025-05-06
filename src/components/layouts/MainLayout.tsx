bcb1a4618cf395aeee678908071d46d4606e8a67
Sionparadox <sion9999@naver.com>
2025-05-06 15:20:39 +0900

    [fix] 레이아웃 요소 위치 변경

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import { Header } from '../organisms/Header';
-import { Sidebar } from '../organisms/Sidebar';
-import { Footer } from '../organisms/Footer';
+import { Header } from './Header';
+import { Sidebar } from './Sidebar';
+import { Footer } from './Footer';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { sidebarAtom, setSidebarToggleAtom } from '@/atoms/sidebar';
 import { useEffect, useCallback, useRef } from 'react';
